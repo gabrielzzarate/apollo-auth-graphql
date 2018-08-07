ced5cd278fd2dbcf9467f416b0ef2498dd74978b
Gabriel Zarate <gabrielzarate@gabriels-mbp-2.lan>
2018-08-07 06:58:09 -0400

    authentication mutations setup

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,8 +1,19 @@
 const graphql = require('graphql');
-const { GraphQLObjectType } = graphql;
+const { GraphQLObjectType, GraphQLID } = graphql;
+const mutation = require('../mutations');
+const UserType = require('./user_type');
 
 const RootQueryType = new GraphQLObjectType({
-  name: 'RootQueryType'
+  name: 'RootQueryType',
+  description: 'Root queries',
+  fields: {
+    user: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        return req.user;
+      }
+    }
+  }
 });
 
 module.exports = RootQueryType;
